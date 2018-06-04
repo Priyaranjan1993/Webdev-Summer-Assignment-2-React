31759226b65ad1e4d969350bb4bb3083aef046e2
Priyaranjan1993 <priyaranjan9090@gmail.com>
2018-06-04 19:14:16 -0400

    topic view bug commit

--- src/containers/modules/ModuleEditor.js
+++ src/containers/modules/ModuleEditor.js
@@ -77,6 +77,7 @@ export default class ModuleEditor extends React.Component {
             .then(() => {
                 this.findLessonForModule(cid, mid);
                 alert("Lesson deleted");
+                this.setState({topics: []});
                 this.setState({
                     topicInput: {
                         title:''
